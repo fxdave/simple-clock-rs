268675667ebf4a9481e3ea12d85f3ce19c84cfe9
David Judge <dbiro97@gmail.com>
2020-03-29 20:05:46 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() -> Result<(), String> {
         .height();
     let pos_x = margin;
     let pos_y = screen_height - margin - height;
-    let bg = Color(0.0, 0.0, 0.0, 0.3);
+    let bg = Color(30.0 / 255.0, 30.0 / 255.0, 30.0 / 255.0, 0.3);
     let fg = Color(1.0, 1.0, 1.0, 1.0);
 
     // Show clock
@@ -36,19 +36,19 @@ fn main() -> Result<(), String> {
     window_builder.set_x(pos_x as u32);
     window_builder.set_y(pos_y as u32);
 
-    let win = window_builder.build()?; // waits for expose then sets props again
+    let win = window_builder.build()?; // waits for expose
 
     let gc = win.get_graphics_context();
+    gc.rounded_rectange(
+        0 as f64,
+        0 as f64,
+        width as f64,
+        height as f64,
+        height as f64 / 5.0,
+    );
 
     let tick = periodic_ms(100);
     loop {
-        gc.rounded_rectange(
-            0 as f64,
-            0 as f64,
-            width as f64,
-            height as f64,
-            height as f64 / 5.0,
-        );
         gc.set_source_rgba(bg.0, bg.1, bg.2, bg.3);
         gc.fill_preserve();
         gc.set_source_rgba(fg.0, fg.1, fg.2, fg.3);
